23340e6bdaa0e9ee1af574956490331c5ae28439
Milly <benitezmilly3@gmail.com>
2025-05-27 11:19:31 -0500

    Configuracion CORS

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
